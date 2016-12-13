156ec0642295f582e8eef1bfcf64dd0116813241
esillen <esillen@kth.se>
2016-12-13 18:43:44 +0100

    Tetris ish går att spela!

---
+++ Assets/Block.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Block {
+
+    public static bool[,] I = new bool[,] { { false, false, false, false }, { true, true, true, true }, { false, false, false, false }, { false, false, false, false } };
+    public static bool[,] J = new bool[,] { { true, false, false }, { true, true, true }, { false, false, false } };
+    public static bool[,] L = new bool[,] { { false, false, true }, { true, true, true }, { false, false, false } };
+    public static bool[,] O = new bool[,] { { true, true }, { true, true } };
+    public static bool[,] S = new bool[,] { { false, true, true }, { true, true, false }, { false, false, false } };
+    public static bool[,] T = new bool[,] { { false, true, false }, { true, true, true }, { false, false, false } };
+    public static bool[,] Z = new bool[,] { { true, true, false }, { false, true, true }, { false, false, false } };
+
+    public bool[,] the_array;
+    public List<GameObject> sprites;
+    public int xpos = 4, ypos = 5;
+    public GameObject block_transform;
+
+    public Block(string type, Transform t_parent, GameObject orig_sprite) {
+        switch (type){
+            case "I":
+                the_array = I.Clone() as bool[,];
+                break;
+            case "J":
+                the_array = J.Clone() as bool[,];
+                break;
+            case "L":
+                the_array = L.Clone() as bool[,];
+                break;
+            case "O":
+                the_array = O.Clone() as bool[,];
+                break;
+            case "S":
+                the_array = S.Clone() as bool[,];
+                break;
+            case "T":
+                the_array = T.Clone() as bool[,];
+                break;
+            case "Z":
+                the_array = Z.Clone() as bool[,];
+                break;
+            default:
+                Debug.LogError("ERRRROOOOOORRR");
+                break;
+        }
+        block_transform = new GameObject();
+        block_transform.name = "current_block";
+        block_transform.transform.parent = t_parent;
+
+        sprites = new List<GameObject>();
+        for(int i=0; i< the_array.GetLength(0); i++){
+            for(int j=0;j<the_array.GetLength(1);j++){
+                if (the_array[i,j]){
+                    GameObject derrr = GameObject.Instantiate(orig_sprite, new Vector3(xpos + j, ypos + i, 0), Quaternion.identity) as GameObject;
+                    derrr.transform.parent = block_transform.transform;
+                    sprites.Add(derrr);
+                }
+            }
+        }
+    }
+
+
+
+    public bool Move(int xdir, int ydir)
+    {
+        xpos += xdir;
+        ypos += ydir;
+        //Check if the move is possible
+        for (int i = 0; i < the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < the_array.GetLength(1); j++)
+            {
+                if (the_array[i, j])
+                {
+                    if (xpos + j < 0 || ypos + i < 0 || xpos + j >= GameField.game_field.GetLength(1) || ypos + i >= GameField.game_field.GetLength(0) || GameField.game_field[ypos + i, xpos + j]){
+                        //Collission
+                        xpos -= xdir;
+                        ypos -= ydir;
+                        Debug.Log("Collission");
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return true;
+
+    }
+
+    public bool Rotate(){ //Only clockwise possible??
+        //Do some crazy rotations!
+        bool[,] transposed_array = new bool[the_array.GetLength(0), the_array.GetLength(1)];
+        bool[,] flipped_array = new bool[the_array.GetLength(0), the_array.GetLength(1)];
+        //Transpose!
+        for (int i = 0; i < the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < the_array.GetLength(1); j++)
+            {
+                transposed_array[i, j] = the_array[j, i];
+            }
+        }
+        //Flip!
+        for (int i = 0; i < the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < the_array.GetLength(1); j++)
+            {
+                flipped_array[i, j] = transposed_array[i, the_array.GetLength(1) -1 - j];
+                if (flipped_array[i, j])
+                {
+                    if (xpos + j < 0 || ypos + i < 0 || xpos + j >= GameField.game_field.GetLength(1) || ypos + i >= GameField.game_field.GetLength(0) || GameField.game_field[ypos + i, xpos + j])
+                    {
+                        Debug.Log("WTF");
+                        return false; //Collission!!!
+                    }
+                }
+            }
+        }
+        //Set as the boss array!
+        for (int i = 0; i < the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < the_array.GetLength(1); j++)
+            {
+                the_array[i, j] = flipped_array[i, j];
+            }
+        }
+        return true;
+    }
+
+    public void UpdateVisuals(){
+        int sprite_counter = 0;
+        for (int i = 0; i < the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < the_array.GetLength(1); j++)
+            {
+                if (the_array[i, j]) {
+                    sprites[sprite_counter].transform.position = new Vector3( xpos + j, ypos + i, 0);
+                    sprite_counter++;
+                }
+            }
+        }
+    }
+
+
+    
+
+
+
+
+}
+
---
+++ Assets/GameField.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameField : MonoBehaviour {
+
+    public static bool [,] game_field = new bool[20,10];
+    public List<GameObject> sprites;
+    public GameObject orig_sprite;
+    public Block selected_block;
+    public GameObject t_game_field;
+    public GameObject t_surrounding;
+    private static List<string> letters = new List<string>() {"I","J","L","O","T","Z","S"};
+
+    public void Start(){
+        t_game_field = new GameObject();
+        t_game_field.name = "game_field";
+        t_game_field.transform.parent = transform.parent;
+        t_surrounding = new GameObject();
+        t_surrounding.name = "surrounding";
+        t_surrounding.transform.parent = transform.parent;
+        selected_block = new Block("J", transform, orig_sprite);
+
+        for (int i = -1; i< game_field.GetLength(0)+1; i++){
+            for (int j = -1; j< game_field.GetLength(1)+1; j++){
+                if (i < 0 || i == game_field.GetLength(0) || j < 0 || j == game_field.GetLength(1)) {
+                    GameObject g = Instantiate(orig_sprite, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
+                    g.transform.parent = t_surrounding.transform;
+                    g.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+            }
+        }
+    }
+
+
+    public void Update() {
+        sprites = new List<GameObject>();
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            if (selected_block.Move(-1, 0)) {
+                selected_block.UpdateVisuals();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (selected_block.Move(1, 0))
+            {
+                selected_block.UpdateVisuals();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (selected_block.Move(0, 1))
+            {
+                selected_block.UpdateVisuals();
+            }
+            else {
+                MergeBlockWithField(selected_block);
+                selected_block = new Block(letters[Random.Range(0,6)],transform, orig_sprite);
+            
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (selected_block.Move(0, -1))
+            {
+                selected_block.UpdateVisuals();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (selected_block.Rotate())
+            {
+                selected_block.UpdateVisuals();
+            }
+        }
+    }
+
+    public void MergeBlockWithField(Block b) {
+        Destroy(b.block_transform);
+
+        b.sprites.Clear();
+        for (int i = 0; i < b.the_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < b.the_array.GetLength(1); j++)
+            {
+                if (b.the_array[i,j]){
+                    game_field[b.ypos + i, b.xpos + j] = true;
+                }
+            }
+        }
+
+        //Collapse rows if possible
+        int current_row = game_field.GetLength(0)-1;
+        int temp_score = 0;
+        while (current_row > 0) {
+            //If entire row is full, move everything down.
+            int counter = 0;
+            for (int j = 0; j < game_field.GetLength(1); j++) { 
+                if (game_field[current_row, j]){
+                    counter++;
+                }
+            }
+            if (counter == game_field.GetLength(1))
+            {
+                temp_score++;
+                //Move everything above down!
+                for (int i = current_row; i > 0; i--)
+                {
+                    for (int j = 0; j < game_field.GetLength(1); j++)
+                    {
+                        game_field[i, j] = game_field[i - 1, j];
+                    }
+                }
+                //Uppermost row is cleared
+                for (int j = 0; j < game_field.GetLength(1); j++)
+                {
+                    game_field[0, j] = false;
+                }
+            }
+            else {
+                current_row--;
+            }
+        }
+        Debug.Log("Scored " + temp_score + " rows!");
+        UpdateVisuals();
+        
+    }
+
+    private void UpdateVisuals() { 
+        //Stupid solution
+        foreach (Transform child in t_game_field.transform) {
+            Destroy(child.gameObject);
+        }
+        sprites.Clear();
+        for (int i = 0; i < game_field.GetLength(0); i++) {
+            for (int j = 0; j < game_field.GetLength(1); j++)
+            {
+                if (game_field[i, j])
+                {
+                    GameObject sprite = Instantiate(orig_sprite, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
+                    sprites.Add(sprite);
+                    sprite.GetComponent<SpriteRenderer>().color = Color.gray;
+                    sprite.transform.parent = t_game_field.transform;
+                }
+
+            }
+        }
+
+
+    
+    }
+
+
+
+
+}
